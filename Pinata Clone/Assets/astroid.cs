638dc5229f327955ba5c406d40114c2de6ab5ae3
Yacine ch <chix88@gmail.com>
2019-11-02 20:04:51 +0100

    spawning asteroid
    need to fix the time to spawn

---
+++ Pinata Clone/Assets/astroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astroid : MonoBehaviour
+{
+    public GameObject asteriodPrefabs;
+    private float respawnTime = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnEnemy();
+        
+    }
+    private void SpawnEnemy()
+    {
+        GameObject a = Instantiate(asteriodPrefabs);
+    }
+}
