81e3caab88982bf892491a41f35c3e85495577af
Yacine ch <chix88@gmail.com>
2019-11-02 02:10:38 +0100

    First Commite
    everything work fine , the background,the player,the floor and the movement work as expected for now

---
+++ Pinata Clone/Assets/characterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float speed;
+    private bool direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+       direction = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int directionOfWalking = Direction(direction);
+        controller.Move(speed * directionOfWalking *Time.fixedDeltaTime, false, false);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = !direction;
+    }
+    private int Direction(bool direction)
+    {
+        if (direction)
+            return 1;
+        else
+            return -1;
+    }
+}
